d5048f3f937b4fe51e20f3023d6e5cb3bf6251a4
Fatush <afromipa55@gmail.com>
2020-02-18 00:40:21 +0200

    refactoring store

--- config/init.php
+++ config/init.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DEBUG', 0);
+define('DEBUG', 1);
 
 define('LAYOUT', 'luxury');
 
@@ -12,7 +12,7 @@ define('CORE', ROOT . '/vendor/luxury/core');
 define('LIBS', ROOT . '/vendor/luxury/core/libs');
 define('CACHE', ROOT . '/tmp/cache');
 
-$app_path = "https://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
 $app_path = preg_replace("#[^/]+$#", '', $app_path);
 $app_path = str_replace("/public/", '', $app_path);
 
